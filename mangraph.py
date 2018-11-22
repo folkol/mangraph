8cdb8bc36dc6d433a3e5e297fb216c89a21a1026
Matte <mattias4@kth.se>
2018-11-22 07:27:58 +0100

    Tidied up the code a bit, in particular moved "seen" into related_tools.

--- mangraph.py
+++ mangraph.py
@@ -5,15 +5,20 @@ import subprocess
 import sys
 
 
-def related_tools(parent, seen):
-    cmd = f'man 1 {parent} | col -bx | sed -n "/SEE ALSO/{{n;p;}}"'
-    related = subprocess.check_output(cmd, shell=True).strip().decode()
-    for tool in re.findall('(\w+)\(1\)', related):
-        edge = f'{parent} -> {tool}'
-        if edge not in seen:
-            seen.add(edge)
-            related_tools(tool, seen)
-    return seen
+def related_tools(root):
+    seen = set()
+
+    def related_tools_recursive(parent):
+        cmd = f'man 1 {parent} | col -bx | sed -n "/SEE ALSO/{{n;p;}}"'
+        related = subprocess.check_output(cmd, shell=True).strip().decode()
+        for tool in re.findall('(\w+)\(1\)', related):
+            edge = (parent, tool)
+            if edge not in seen:
+                seen.add(edge)
+                related_tools_recursive(tool)
+        return seen
+
+    return related_tools_recursive(root)
 
 
 def main():
@@ -23,11 +28,11 @@ def main():
 
     tool = sys.argv[1]
 
-    edges = related_tools(tool, set())
+    edges = related_tools(tool)
     print('digraph {')
-    print(f'    {tool} [peripheries=2];')
-    for edge in edges:
-        print(f'    {edge};')
+    print(f'  {tool} [peripheries=2];')
+    for a, b in edges:
+        print(f'  {a} -> {b};')
     print('}')
 
 
