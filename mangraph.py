69fbd0511b9ea2cf7a25d9f9c7a9231051ec0946
Matte <mattias4@kth.se>
2019-06-09 13:22:26 +0200

    Added most.{dot,png}, with roots from /usr/{sbin,bin} and with Bash builtins removed

--- mangraph.py
+++ mangraph.py
@@ -5,34 +5,151 @@ import subprocess
 import sys
 
 
-def related_tools(root):
+BUILTINS = [
+    'builtin',
+    '!',
+    '%',
+    '.',
+    ':',
+    '@',
+    '{',
+    '}',
+    'alias',
+    'alloc',
+    'bg',
+    'bind',
+    'bindkey',
+    'break',
+    'breaksw',
+    'builtins',
+    'case',
+    'cd',
+    'chdir',
+    'command',
+    'complete',
+    'continue',
+    'default',
+    'dirs',
+    'do',
+    'done',
+    'echo',
+    'echotc',
+    'elif',
+    'else',
+    'end',
+    'endif',
+    'endsw',
+    'esac',
+    'eval',
+    'exec',
+    'exit',
+    'export',
+    'false',
+    'fc',
+    'fg',
+    'filetest',
+    'fi',
+    'for',
+    'foreach',
+    'getopts',
+    'glob',
+    'goto',
+    'hash',
+    'hashstat',
+    'history',
+    'hup',
+    'if',
+    'jobid',
+    'jobs',
+    'kill',
+    'limit',
+    'local',
+    'log',
+    'login',
+    'logout',
+    'ls-F',
+    'nice',
+    'nohup',
+    'notify',
+    'onintr',
+    'popd',
+    'printenv',
+    'pushd',
+    'pwd',
+    'read',
+    'readonly',
+    'rehash',
+    'repeat',
+    'return',
+    'sched',
+    'set',
+    'setenv',
+    'settc',
+    'setty',
+    'setvar',
+    'shift',
+    'source',
+    'stop',
+    'suspend',
+    'switch',
+    'telltc',
+    'test',
+    'then',
+    'time',
+    'times',
+    'trap',
+    'true',
+    'type',
+    'ulimit',
+    'umask',
+    'unalias',
+    'uncomplete',
+    'unhash',
+    'unlimit',
+    'unset',
+    'unsetenv',
+    'until',
+    'wait',
+    'where',
+    'which',
+    'while'
+]
+
+
+def related_tools(roots):
     seen = set()
 
     def related_tools_recursive(parent):
         cmd = f'man 1 {parent} | col -bx | sed -n "/SEE ALSO/{{n;p;}}"'
         related = subprocess.check_output(cmd, shell=True).strip().decode()
         for tool in re.findall('(\w+)\(1\)', related):
+            if tool in BUILTINS:
+                continue
             edge = (parent, tool)
             if edge not in seen:
+                print(f'Following {edge[0]} -> {edge[1]}', file=sys.stderr)
                 seen.add(edge)
                 related_tools_recursive(tool)
         return seen
 
-    return related_tools_recursive(root)
+    for root in roots:
+        related_tools_recursive(root)
+    return seen
 
 
 def main():
-    if len(sys.argv) != 2:
-        print('usage: python mangraph.py tool | dot')
+    if len(sys.argv) < 2:
+        print('usage: python mangraph.py tools... | dot')
         sys.exit(1)
 
-    tool = sys.argv[1]
+    tools = sys.argv[1:]
 
-    edges = related_tools(tool)
+    edges = related_tools(tools)
     print('digraph {')
-    print(f'  {tool} [peripheries=2];')
+    #for tool in tools:
+        #print(f'  {tool} [peripheries=2];')
     for a, b in edges:
-        print(f'  {a} -> {b};')
+        print(f'  "{a}" -> "{b}";')
     print('}')
 
 
